a6998ceb9e92cad143c7c1d62a379e26fa7bd540
Fznaf <89885429+Fznaf@users.noreply.github.com>
2022-11-02 12:38:19 +0700

    Add files via upload

---
+++ Interface/src/interfaceAbstract/InterfaceBankAccount.java
@@ -0,0 +1,10 @@
+package interfaceAbstract;
+
+interface InterfaceBankAccount {
+    public final String BANK = "JavaBank Indonesia";
+
+    public void deposit(int amt);
+    public void withdraw(int amt);
+    public int getBalance();
+    public String getBankName();
+}
